676650e5b4778f83a80396b912943308c2ecaff4
diogosctn <diogosctn@gmail.com>
2022-09-14 10:49:06 -0300

    Change scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/server
-yarn
-yarn add react react-dom react-scripts react-particles-js
-yarn global add pm2
+
+ls -a .
+ls -a ..
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/server
+ls -a .
+ls -a ..
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs yarn
+yum -y install nodejs npm
+npm install serve -g
+
+
